17e9fe2781955bebc495ab0e93a959d37874976d
Qesem1999 <46812936+Qesem1999@users.noreply.github.com>
2019-05-05 13:57:51 +0400

    Add files via upload

---
+++ GenericSolution/generic/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic
+{
+    class Employee :User
+    {
+        protected static int id = 0;
+        public string Position { get; set; }
+        public double Salary { get; set; }
+
+        public int getId()
+        {
+            return ++id;
+        }
+
+    
+
+    }
+}
---
+++ GenericSolution/generic/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+    }
+}
